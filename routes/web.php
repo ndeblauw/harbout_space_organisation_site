26c7104043e311fee53fff8706f8e6c66a6c76be
Nico Deblauwe <nico@bluepundit.eu>
2022-01-17 10:00:57 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', \App\Http\Controllers\WelcomeController::class);
 
 Route::get('test', function () {
-    abort(401);
-    //throw new \Exception('error Nico morning now awaken');
+    //abort(401);
+    throw new \Exception('error Nico morning now awaken');
 });
 
 Route::get('/dashboard', function () {
