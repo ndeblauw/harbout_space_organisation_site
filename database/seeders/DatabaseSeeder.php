360cf0b1f5ab99dffefd5fffaea0caac149c8f10
Nico Deblauwe <nico@bluepundit.eu>
2022-01-18 20:05:40 +0100

    add seeder (as described in readme.MD)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Activity;
+use App\Models\Location;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Location::factory(10)->create();
+        Activity::factory(20)->create();
     }
 }
