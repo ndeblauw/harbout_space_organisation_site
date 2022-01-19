d699d43fa593ef917c1b158b28a5fe98c8c87a4d
Nico Deblauwe <nico@bluepundit.eu>
2022-01-19 12:23:58 +0100

    adding support for ES

---
+++ app/Http/Middleware/LocaleSetter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class LocaleSetter
+{
+    public function handle(Request $request, Closure $next)
+    {
+        ray($request);
+
+        if($request->getHost() == 'organisation_es.test') {
+            app()->setLocale('es');
+        }
+
+        return $next($request);
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 
         <!-- Hero or so-->
         <div class="w-100 h-40 bg-gradient-to-br from-blue-400 to-teal-200 text-white p-8">
-            <h1 class="block text-4xl font-bold">Great slogan comes here</h1>
-            <h2 class="block text-2xl font-semi-bold text-blue-50">and a tagline or explanation is printed here</h2>
+            <h1 class="block text-4xl font-bold"> {{ __('welcome.slogan') }}</h1>
+            <h2 class="block text-2xl font-semi-bold text-blue-50">{{ __('and a tagline or explanation is printed here') }}</h2>
         </div>
 
         <!-- main content -->
